d4ee05fc9d959829d5695d8bb9df13a6a5defe34
user <user@Users-MacBook-Pro.local>
2023-09-05 21:32:41 +0600

    created a route for login

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -24,4 +24,31 @@ usersRouter.post('/', async (req, res, next) => {
     }
 });
 
+usersRouter.post('/sessions', async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+
+        if (!user) {
+            return res.status(400).send({error: 'Username not found'});
+        }
+
+        const isMatch = await user.checkPassword(req.body.password);
+
+        if (!isMatch) {
+            return res.status(400).send({error: 'Password is wrong'});
+        }
+
+        user.generateToken();
+
+        await user.save();
+        return res.send({message: 'Username and password correct!', user});
+    } catch (error) {
+        if (error instanceof Error.ValidationError) {
+            return res.status(400).send(error);
+        }
+
+        return next(error);
+    }
+});
+
 export default usersRouter;
